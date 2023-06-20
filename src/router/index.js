b9c8b6b07eb7ed189e11441e2ac65142488b2fc3
keisuzuki <kei.nishimura0131@gmail.com>
2023-06-20 21:27:28 +0900

    VueGitHub_test

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import IssueList from '../views/IssueList.vue'
+import TodoList from '../views/TodoList.vue'
+import TodoIssues from '../views/TodoIssues.vue'
+
 
 Vue.use(VueRouter)
 
@@ -11,13 +15,20 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/todo-list',
+    name: 'TodoList',
+    component: TodoList
+  },
+  {
+    path: '/todos-issues',
+    name: 'TodoIssues',
+    component: TodoIssues
+  },
+  {
+    path: '/issue-list',
+    name: 'IssueList',
+    component: IssueList
+  },
 ]
 
 const router = new VueRouter({
